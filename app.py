207889526726e3bb94bf490390baeb34a18a10d1
VINITGG <128659073+VINITGG@users.noreply.github.com>
2025-01-23 23:00:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import logging
+from flask import Flask, render_template
+
+# Setting up logging
+logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    logging.info("Home page accessed")
+    return render_template("index.html")  
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
